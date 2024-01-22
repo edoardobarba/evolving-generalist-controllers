ea365aa2af99fabd7a3087e8052dc5294b5802d8
Edoardo Barba <edoardo.barba1999@gmail.com>
2024-01-22 15:00:07 +0100

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -21,32 +21,36 @@ from cartpole_modified import CartPoleEnv
 import gymnasium as gym
 #from utils import gym_render, save_dataframes
 import joblib
+from gym.wrappers.record_video import RecordVideo
 
 
-train_cauchy1_path = r"C:\Users\edoardo.barba\Results_Biped\cauchy1\20231213105259"
-train_cauchy2_path = r"C:\Users\edoardo.barba\Results_Biped\cauchy2\20231213105259"
-train_gaussian1_path = r"C:\Users\edoardo.barba\Results_Biped\gaussian1\20231212113730"
-train_gaussian2_path = r"C:\Users\edoardo.barba\Results_Biped\gaussian2\20231212113730"
-train_incremental_path = 'C:\\Users\\edoar\\Documents\\GitHub\\Results_Biped\\incremental\\20231212113730'
-train_uniform_path = r"C:\Users\edoardo.barba\Results_Biped\uniform\20231213105259"
-train_RL_path = r"C:\Users\edoardo.barba\Results_Biped\RL\20231212113849"
-train_beta1 = "C:\\Users\\edoar\\Documents\\GitHub\\Results_Biped\\beta1\\20231215114653"
-train_beta2 = r"C:\Users\edoardo.barba\Results_Biped\beta2\20231213105322"
-train_betawalk1 = r"C:\Users\edoardo.barba\Results_Biped\betawalk1\20231212114325"
-train_betawalk2 = r"C:\Users\edoardo.barba\Results_Biped\betawalk2\20231212114325"
+# BIPEDAL WALKER 
 
+train_cauchy1_path = "/home/edoardo.barba/Results_Biped/cauchy1/20231219091844"
+train_cauchy2_path = "/home/edoardo.barba/Results_Biped/cauchy2/20231219091844"
+train_gaussian1_path = r'C:\Users\edoar\Documents\GitHub\evolving-generalist-controllers\Results_Biped\gaussian1\20231223120018'
+train_gaussian2_path = "/home/edoardo.barba/Results_Biped/gaussian2/20231223120018"
+train_incremental_path = "/home/edoardo.barba/Results_Biped/incremental/20231219025742"
+train_uniform_path = "/home/edoardo.barba/Results_Biped/uniform/20231219091844"
+train_RL_path = "/home/edoardo.barba/Results_Biped_old/RL/20231215114621" 
+train_beta01 = r'C:\Users\edoar\Documents\GitHub\evolving-generalist-controllers\Results_Biped\beta01\20231219092337'
+train_beta02 = "/home/edoardo.barba/Results_Biped/beta02/20231219092337"
+train_betawalk01 = "/home/edoardo.barba/Results_Biped/betawalk01/20231221112601" 
+train_betawalk02 = "/home/edoardo.barba/Results_Biped/betawalk02/20231221112601" 
+# train_gauss_dec = "/home/edoardo.barba/Results_Biped/gauss_dec/20231223120018"
+train_default_path = "/home/edoardo.barba/Results_Biped/default/20240103141803"
+train_borderincr_path = "/home/edoardo.barba/Results_Biped/border_incr/20240103134750"
+train_random_path = "/home/edoardo.barba/Results_Biped/random/20240108110555"
 
-# training_schedules = ["incremental", "gaussian1", "gaussian2", "cauchy1", "cauchy2", "uniform", "RL", "beta1", "beta2", "betawalk1", "betawalk2"]
-# all_train_folders = [train_incremental_path, train_gaussian1_path, train_gaussian2_path, train_cauchy1_path, train_cauchy2_path, train_uniform_path, train_RL_path, train_beta1, train_beta2, train_betawalk1, train_betawalk2]
-training_schedules = ["beta1"]
-all_train_folders = [train_beta1]
+training_schedules = ["gaussian1", "beta01"]
+all_train_folders = [train_gaussian1_path, train_beta01]
 
 
 ACTORS = -1
 
 SEED=0
 
-def gym_render_testing(game, agent, xml_path, parameters, topology, steps):
+def gym_render_testing(game, agent, xml_path, parameters, topology, steps, schedule_name):
     s = 0
     total_reward = 0
 
@@ -61,7 +65,11 @@ def gym_render_testing(game, agent, xml_path, parameters, topology, steps):
         env.LINK_MASS_1 = parameters[0]  #: [kg] mass of link 1
         env.LINK_MASS_2 = parameters[1]  #: [kg] mass of link 2
     else:
-        env = game(parameters, render_mode = "human")
+        env = game(parameters, render_mode = "rgb_array")
+
+    # Wrap the environment with the Monitor wrapper
+    name_folder = os.path.join("./video", schedule_name)
+    env = RecordVideo(env, name_folder, episode_trigger=lambda episode_id: True, name_prefix=str(parameters))
 
     obs, info = env.reset(seed=s)
     done = False
@@ -89,10 +97,10 @@ def gym_render_testing(game, agent, xml_path, parameters, topology, steps):
     #env.close()
     return -total_reward
 
-def comparison(game, agent, i, test_set, topology, max_steps, xml_path = None):
+def comparison(game, agent, i, test_set, topology, max_steps, schedule_name, xml_path = None):
     print("ENV: ")
     print(test_set[i])
-    fitness = gym_render_testing(game, agent, xml_path, test_set[i], topology, max_steps)
+    fitness = gym_render_testing(game, agent, xml_path, test_set[i], topology, max_steps, schedule_name)
     print("fitness: ")
     print(fitness)
     return fitness
@@ -111,7 +119,7 @@ def get_NN(file):
     return nn, weights
 
 
-def set_test(config, nn, weights, max_steps, max_fitness, testing_set): 
+def set_test(config, nn, weights, max_steps, max_fitness, testing_set, schedule_name): 
     game = config["game"]
     topology = config["NN-struc"]
     #print("testing ", testing_set, "...")
@@ -123,7 +131,7 @@ def set_test(config, nn, weights, max_steps, max_fitness, testing_set):
 
     history_reward = []    
     
-    history_reward = joblib.Parallel(n_jobs=1)(joblib.delayed(comparison)(game = game, agent=nn, i=i, test_set=all_variations, topology=topology, max_steps = max_steps)
+    history_reward = joblib.Parallel(n_jobs=1)(joblib.delayed(comparison)(game = game, agent=nn, i=i, test_set=all_variations, topology=topology, max_steps = max_steps, schedule_name=schedule_name)
                                                                 for i in range(len(all_variations)))
     
     return history_reward, all_variations
@@ -142,7 +150,7 @@ if __name__ == '__main__':
     
     json_file_name = str(sys.argv[1])
 
-    for train_folder_path in all_train_folders:
+    for i, train_folder_path in enumerate(all_train_folders):
         print(train_folder_path)
         save_path = train_folder_path
         runs_folder_path = save_path + "\\runs"
@@ -164,25 +172,27 @@ if __name__ == '__main__':
         all_history_rewards_OUT = []
         all_history_rewards_INOUT = []
 
-        #generations = 3000
-        #print("Testing ", generations, "generations")
+        generations = 4000
+        print("Testing ", generations, "generations")
         for i, run_number in enumerate(runs_folders):
+            
             run_path = os.path.join(runs_folder_path, run_number)
-            training_txt_path = os.path.join(run_path,"training.txt")
-            with open(training_txt_path, 'r') as file:
-                content = file.read()
-                generations_number = int(content[13:-1])
-                print("gen_number: ", generations_number)
+            generations_path = os.path.join(run_path, str(generations))
+            # training_txt_path = os.path.join(run_path,"training.txt")
+            # with open(training_txt_path, 'r') as file:
+            #     content = file.read()
+            #     generations_number = int(content[13:-1])
+            #     print("gen_number: ", generations_number)
 
             print("Testing run number", i+1, "..." )
             
-            generalist_folder_path = os.path.join(run_path, "evals")
+            generalist_folder_path = os.path.join(generations_path, "evals")
             path_generalist_ANN_weights = os.path.join(generalist_folder_path, os.listdir(generalist_folder_path)[0])
             file_generalist_ANN_weights = open(path_generalist_ANN_weights)
             nn, weights = get_NN(file_generalist_ANN_weights)
             file_generalist_ANN_weights.close()
 
-            history_reward_INOUT, all_var_INOUT = set_test(config, nn = nn, weights=weights, max_steps=max_steps, max_fitness=max_fitness, testing_set="INOUT")  
+            history_reward_INOUT, all_var_INOUT = set_test(config, nn = nn, weights=weights, max_steps=max_steps, max_fitness=max_fitness, testing_set="INOUT", schedule_name=training_schedules[i])  
 
             all_history_rewards_INOUT.append(np.array(history_reward_INOUT))
 
